0603d4d9096b30fdb309a2ef7f1ce5aac746e662
HiperFelps <162184664+HiperFelps@users.noreply.github.com>
2025-09-15 13:49:35 -0300

    Update tcc.sql

--- Menu/tcc.sql
+++ Menu/tcc.sql
@@ -2,6 +2,7 @@ create database tcc;
 use tcc;
 drop database if exists tcc;
 drop table if exists usuarios;
+drop table if exists administrador;
 
 create table usuarios(
 id int not null auto_increment,
@@ -10,5 +11,13 @@ senha varchar(255) not null,
 numPet int,
 numColor int,
 NumNivel int,
+numEnergia int,
 primary key (id)
-)
+);
+
+create table administrador(
+id int not null auto_increment,
+email varchar(100) not null unique,
+senha varchar(255) not null,
+primary key(id)
+);
